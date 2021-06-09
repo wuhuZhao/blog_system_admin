02fc349f2d439bd74ff011723d62580644a20981
hkzhao123 <1223602915@qq.com>
2021-06-09 22:42:58 +0800

    update login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -9,7 +9,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
+import { connect } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
     paper: {

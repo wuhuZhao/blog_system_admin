9378c555701bad0f641b6e5d1637d1a91148a492
hkzhao123 <1223602915@qq.com>
2021-06-09 13:22:13 +0800

    update login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from "./store";
+import SignIn from "./pages/login";
 function App() {
     return (
         <Provider store={store}>
             <BrowserRouter>
                 <div>
-
+                    <Route path={"/"} exact component={SignIn}/>
                 </div>
             </BrowserRouter>
         </Provider>
